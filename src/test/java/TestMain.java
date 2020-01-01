056c45c1eeaa40136d8d7d6afa6f905e2dc06eb2
rbulin <roman.bulinski@pentacomp.pl>
2020-01-01 15:03:33 +0100

    Created structure of app

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+    @Test
+    public void test_01() {
+        assertEquals(0, Main.sortDesc(0));
+    }
+
+    @Test
+    public void test_02() {
+        assertEquals(51, DescendingOrder.sortDesc(15));
+    }
+
+
+    @Test
+    public void test_03() {
+        assertEquals(987654321, DescendingOrder.sortDesc(123456789));
+    }
+}
